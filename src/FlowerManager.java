49b7193278b5a028fea29145f3ddae91d856a2b4
Přemysl Ježek <132379663+PremyslJ@users.noreply.github.com>
2023-06-25 13:15:56 +0200

    přidáno:  - Seřazení výstupních dat podle názvu květiny (abecedně)  - Seřaď rostliny podle data poslední zálivky  - Najdi a vypiš dny, kdy jsi zasadil(a) alespoň jednu rostlinu  - ve třídě Plant byl plantList smazán  - opravené názvy metod metod fileName a fileName1 na fileNameInput a fileNameOutput. Uznávám, trochu masakr název :-)  - oprava konstruktorů ve třidě"Plant"

--- src/FlowerManager.java
+++ src/FlowerManager.java
@@ -1,20 +1,19 @@
 import java.io.*;
 import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
-public class FlowerManager {
+public class FlowerManager  {
 
-    private List<Plant> plantList = new ArrayList<>();
+    public List<Plant> plantList = new ArrayList<>();
+
 
     //metoda pro načtení květin ze souboru
-    public void loadDataFromFile(String filename, String delimiter) throws PlantException {
+    public void loadDataFromFile(String fileNameInput, String delimiter) throws PlantException {
         String[] items = new String[0];
         String line = "";
         int lineNumber = 1;
-        try (Scanner scanner = new Scanner(new BufferedReader(new FileReader(filename)))) {
+        try (Scanner scanner = new Scanner(new BufferedReader(new FileReader(fileNameInput)))) {
             while (scanner.hasNextLine()) {
                 line = scanner.nextLine();
                 // System.out.println(line);
@@ -29,13 +28,14 @@ public class FlowerManager {
 
                 Plant newPlant = new Plant(name, notes, planted, watering, frequencyOfWatering);
                 plantList.add(newPlant);
+
+
                 lineNumber++;
-
-
             }
+
             // vyjímky
         } catch (FileNotFoundException e) {
-            throw new PlantException("Soubor" + filename + "nebyl nalezen" + e.getLocalizedMessage());
+            throw new PlantException("Soubor" + fileNameInput + "nebyl nalezen" + e.getLocalizedMessage());
         } catch (NumberFormatException e) {
             throw new PlantException(", špatně zadaný znak(není číslo) :   " + items[2] + "  na radku: " + lineNumber + ": " + line);
         } catch (DateTimeParseException e) {
@@ -45,11 +45,11 @@ public class FlowerManager {
     }
 
     //metoda pro uložení květin do souboru
-    public void saveDataToFile(String filename, String delimiter) throws PlantException {
+    public void saveDataToFile(String fileNameOutput, String delimiter) throws PlantException {
         // 1. otevřít soubor pro zápis
         // 2. zapsat data
         // 3. zavřít soubor
-        try (PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter(filename)))) {
+        try (PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter(fileNameOutput)))) {
             for (Plant plant : plantList) {
                 writer.println(
                         plant.getName() + delimiter
@@ -60,12 +60,12 @@ public class FlowerManager {
                 );
             }
         } catch (IOException e) {
-            throw new PlantException("Chyba zápisu do souboru " + filename + ": " + e.getLocalizedMessage());
+            throw new PlantException("Chyba zápisu do souboru " + fileNameOutput + ": " + e.getLocalizedMessage());
         }
     }
 
 
-    // přidání další květiny do seznamu
+    //přidání další květiny do seznamu
     public void add(Plant newPlant) {
 
         plantList.add(newPlant);
@@ -87,4 +87,7 @@ public class FlowerManager {
     public List<Plant> getPlantList() {
         return new ArrayList<>(plantList);
     }
+
+
 }
+
